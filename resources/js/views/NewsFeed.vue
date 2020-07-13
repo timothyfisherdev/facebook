0045b6f732787a2c15215fa12079abbd527a7a8b
Timothy Fisher <tpf27@nau.edu>
2020-07-13 15:23:42 -0700

    Build out the first post component for the news feed

--- resources/js/views/NewsFeed.vue
+++ resources/js/views/NewsFeed.vue
@@ -1,16 +1,20 @@
 <template>
 	<div class="flex flex-col items-center py-4">
 		<AddPost />
+
+		<Post />
 	</div>
 </template>
 
 <script>
 	import AddPost from '../components/AddPost';
+	import Post from '../components/Post';
 
 	export default {
 		name: 'NewsFeed',
 		components: {
-			AddPost
+			AddPost,
+			Post
 		}
 	}
 </script>
