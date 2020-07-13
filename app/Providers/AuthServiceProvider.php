fb49dbe85ac860c371a88169e4d6cef3e843a413
Timothy Fisher <tpf27@nau.edu>
2020-07-13 09:23:30 -0700

    Setup Vue scaffolding

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Laravel\Passport;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
